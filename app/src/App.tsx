7917c41cd855e0f64668ca0c6b9a5c0a594c62ec
Dennis Larsson Hagström <dennis.lh@live.se>
2024-01-15 08:45:23 +0100

    bump stuff, center text

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import "./App.css";
 
 function App() {
-   return (
-      <h1 className="text-3xl font-bold underline text-red-600">
-         Simple React Typescript Tailwind Sample
-      </h1>
-   );
+  return (
+    <h1 className="text-3xl h-screen w-screen flex justify-center items-center font-bold text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
 }
 
 export default App;
