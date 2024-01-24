d31949d2a29c2cedea07dc27b8677c086ac635e6
Dennis Larsson Hagström <dennis.lh@live.se>
2024-01-24 11:14:47 +0100

    did linuss work

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,10 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl h-screen w-screen flex justify-center items-center font-bold text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <div>
+      <h1 className="text-3xl h-screen w-screen flex justify-center items-center font-bold text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+      <DennisComponent text={"Linus should have done this"} />
+    </div>
   );
 }
 
 export default App;
+
+type DennisComponentType = {
+  text: string;
+};
+function DennisComponent({ text }: DennisComponentType) {
+  return <p className="text-blue-600">{text}</p>;
+}
